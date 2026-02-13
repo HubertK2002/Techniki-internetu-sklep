feb2ecd20db8161150b955e5483903c6905f4c58
HubertK2002 <hubistef16@wp.pl>
2026-02-13 14:02:29 +0100

    Ujednolicenie widoku logowania oraz rejestracji z projektem frontend dla formularzy

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -11,36 +11,55 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\EqualTo;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-			->add('firstName')
-			->add('lastName')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue(message: 'You should agree to our terms.'),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank(
-                        message: 'Please enter a password',
-                    ),
-                    new Length(
-                        min: 6,
-                        minMessage: 'Your password should be at least {{ limit }} characters',
-                        max: 4096,
-                    ),
-                ],
-            ])
-        ;
+			->add('email', null, [
+				'label' => 'form.registration.email',
+			])
+			->add('firstName', null, [
+				'label' => 'form.registration.first_name',
+			])
+			->add('lastName', null, [
+				'label' => 'form.registration.last_name',
+			])
+			->add('agreeTerms', CheckboxType::class, [
+				'label' => 'form.registration.agree_terms',
+				'mapped' => false,
+				'constraints' => [
+					new IsTrue(message: 'form.registration.errors.agree_terms'),
+				],
+			])
+			->add('plainPassword', PasswordType::class, [
+				'label' => 'form.registration.password',
+				'mapped' => false,
+				'attr' => ['autocomplete' => 'new-password'],
+				'constraints' => [
+					new NotBlank(message: 'form.registration.errors.password_blank'),
+					new Length(
+						min: 6,
+						minMessage: 'form.registration.errors.password_min',
+						max: 4096,
+					),
+				],
+			])
+			->add('confirmPassword', PasswordType::class, [
+				'label' => 'form.registration.password_confirm',
+				'mapped' => false,
+				'attr' => ['autocomplete' => 'new-password'],
+				'constraints' => [
+					new NotBlank(message: 'form.registration.errors.password_confirm_blank'),
+					new EqualTo([
+						'propertyPath' => 'plainPassword',
+						'message' => 'form.registration.errors.password_confirm_mismatch',
+					]),
+				],
+			])
+		;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
