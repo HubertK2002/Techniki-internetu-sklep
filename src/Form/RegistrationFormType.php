43b972cfd931876ce2d600a9c2bcdb8a1079afea
HubertK2002 <hubistef16@wp.pl>
2026-02-16 10:34:11 +0100

    Poprawa formularza rejestracji

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,7 +14,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\EqualTo;
 
 class RegistrationFormType extends AbstractType
 {
@@ -53,13 +55,19 @@ class RegistrationFormType extends AbstractType
 				'attr' => ['autocomplete' => 'new-password'],
 				'constraints' => [
 					new NotBlank(message: 'form.registration.errors.password_confirm_blank'),
-					new EqualTo([
-						'propertyPath' => 'plainPassword',
-						'message' => 'form.registration.errors.password_confirm_mismatch',
-					]),
 				],
 			])
 		;
+
+		$builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event): void {
+			$form = $event->getForm();
+			$plainPassword = (string) $form->get('plainPassword')->getData();
+			$confirmPassword = (string) $form->get('confirmPassword')->getData();
+
+			if ($plainPassword !== $confirmPassword) {
+				$form->get('confirmPassword')->addError(new FormError('Hasła muszą być takie same.'));
+			}
+		});
     }
 
     public function configureOptions(OptionsResolver $resolver): void
